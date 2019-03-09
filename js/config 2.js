4811f6c01ede0707459781a3d052c8cee7d537a5
Cao Tri Nguyen <caotrisnguyen@gmail.com>
2019-03-09 14:10:39 +0700

    end

--- js/config 2.js
+++ js/config 2.js
@@ -6,8 +6,8 @@ function  collapse(){
     //let gg = document.querySelectorAll("div.gg");
     if (getWidth() < tablet_screen)    {
         element.classList.add("collapse");
-        //document.getElementById("carousel_hidden").classList.remove("d-flex");
-        //document.getElementById("green_back").classList.add("green_back");
+        document.getElementById("carousel_hidden").classList.remove("d-flex");
+        document.getElementById("green_back").classList.add("green_back");
        // gg.classList.remove("d-flex");
     }
     else if (getWidth() < phone_screen) {
@@ -15,8 +15,8 @@ function  collapse(){
     }
     else { 
         element.classList.remove("collapse");
-       // document.getElementById("carousel_hidden").classList.add("d-flex");
-       // document.getElementById("green_back").classList.remove("green_back");
+        document.getElementById("carousel_hidden").classList.add("d-flex");
+        document.getElementById("green_back").classList.remove("green_back");
         //gg.classList.add("d-flex");
         //document.getElementById("header_mobile_dflex").classList.add("d-flex");
 }
